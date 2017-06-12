f81023c888dffdd0772232c642ef6edb6e1bd722
picce <picce@yahoo.it>
2017-06-12 22:58:27 +0200

    init repo

---
+++ LogmebotException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logmebot.Net
+{
+    public class LogMeBotException : Exception
+    {
+
+        private int statusCode;
+        public int StatusCode
+        {
+            set { statusCode = value; }
+            get { return statusCode; }
+        }
+
+        public LogMeBotException(int statusCode, string message) : base(message)
+        {
+            this.StatusCode = statusCode;
+        }
+    }
+}
